d8314a0549f06e08b689fdb0246084756f24e6e8
derek chen <derek.g.chen@gmail.com>
2018-03-25 19:16:15 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -48,7 +48,9 @@ def reformat_languages(languages)
   languages.each do |style, list_lang|
     list_lang.each do |kind_of_lang, type|
       new_array = %i[]
-      new_hash[] =
+      new_array << sytle
+      new_hash[kind_of_lang] = type.merge({style:new_array}) 
+      
 
       puts type
 
